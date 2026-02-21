bf28421c512b71ba1a765e9a9c7e02ecc0603deb
ThyMuoyhak <thymuoyhak.biu@gmail.com>
2026-02-21 21:49:28 +0700

    update

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
+const API_URL = process.env.REACT_APP_API_URL || 'https://backend4-phone-ecommerce.onrender.com';
 
 const Products = ({ filter = 'all', searchQuery: propSearchQuery = '' }) => {
     const [products, setProducts] = useState([]);
