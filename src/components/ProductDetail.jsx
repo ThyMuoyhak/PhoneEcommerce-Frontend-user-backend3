3be1fd802f054762bfeef6087554025df2a93773
ThyMuoyhak <thymuoyhak.biu@gmail.com>
2026-02-21 21:36:18 +0700

    update

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -3,7 +3,7 @@ import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
-const API_URL = 'http://localhost:8000';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
 
 const ProductDetails = () => {
     const { id } = useParams();
